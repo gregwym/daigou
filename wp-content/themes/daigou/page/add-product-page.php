6a4e69b7c09461201eedc0eb39cc9e4297a61b4d
ShengMin Zhang <me@shengmin.me>
2013-05-20 21:03:45 -0400

    require login on the page

--- wp-content/themes/daigou/page/add-product-page.php
+++ wp-content/themes/daigou/page/add-product-page.php
@@ -5,18 +5,22 @@
  * This page allows users to paste in a Tao Bao product URL, and add that product to shopping cart.
  */
 
-	require_once(__DIR__ . '/../ui/ProductUrlInput.php');
-	require_once(__DIR__ . '/../lib/Configuration.php');
+if (!is_user_logged_in()) {
+  auth_redirect(); 
+} 
 
-	class AddProductPage {
-		public static function onEnqueueScripts() {
-			$js = \daigou\Configuration::getJavaScriptDirectory();
+require_once(__DIR__ . '/../ui/ProductUrlInput.php');
+require_once(__DIR__ . '/../lib/Configuration.php');
 
-			wp_enqueue_script('daigou.add-product-page', $js . '/add-product-page.js', array('jquery', 'daigou.ProductUrlInput'));
-		}
-	}
+class AddProductPage {
+  public static function onEnqueueScripts() {
+    $js = \daigou\Configuration::getJavaScriptDirectory();
 
-	add_action('wp_enqueue_scripts', array('AddProductPage', 'onEnqueueScripts'));
+    wp_enqueue_script('daigou.add-product-page', $js . '/add-product-page.js', array('jquery', 'daigou.ProductUrlInput'));
+  }
+}
+
+add_action('wp_enqueue_scripts', array('AddProductPage', 'onEnqueueScripts'));
 ?>
 <?php get_header(); ?>       
 <div id="content" class="page col-full" style="height: 600px;">
