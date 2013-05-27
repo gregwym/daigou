5eb700f9a4ce73c42e3fb45d3727b81ec7c17107
ShengMin Zhang <me@shengmin.me>
2013-05-27 01:34:49 -0400

    rename taobao-url plugin to daigou

--- wp-content/plugins/daigou/daigou.php
+++ wp-content/plugins/daigou/daigou.php
@@ -4,9 +4,8 @@
  * @version 0.1
  */
 /*
-Plugin Name: Taobao URL
-Plugin URI: https://github.com/gregwym/wp-taobao-url
-Description: WordPress plugin for Taobao URL parsing and Product information fetching.
+Plugin Name: Daigou
+Description: WordPress plugin for Daigou
 Author: Greg Wang & ShengMin Zhang
 Version: 0.1
 Author URI: http://sample.com/
