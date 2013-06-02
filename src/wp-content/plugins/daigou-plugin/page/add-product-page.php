b2d8f03f314adb27eb622e39ece60fb5fb08b45e
ShengMin Zhang <me@shengmin.me>
2013-06-02 16:02:40 -0400

    fine tune style of taobao url input box

--- src/wp-content/plugins/daigou-plugin/page/add-product-page.php
+++ src/wp-content/plugins/daigou-plugin/page/add-product-page.php
@@ -5,6 +5,6 @@
  * This page allows users to paste in a Tao Bao product URL, and add that product to shopping cart.
  */
 ?>
-<div id="taobao-add-product" style="height: 600px;">
+<div id="taobao-add-product" style="min-height: 100px">
 
 </div><!-- /#content -->
